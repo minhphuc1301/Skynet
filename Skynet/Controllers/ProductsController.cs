e149a969cff48baebeec40ca677da1fa67fa5517
minhphuc1301 <nhoclunway@gmail.com>
2023-09-22 15:07:55 +0700

    End of section 2

---
+++ Skynet/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Core.Models;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Skynet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null) return product;
+            return NotFound("Product not found");
+        }
+    }
+}
